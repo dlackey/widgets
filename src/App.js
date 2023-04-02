042c8ae9f6b97ad44b762789478d6d46f32b111d
David Lackey <dmlackey@icloud.com>
2023-04-02 16:45:55 -0500

    Added timer

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
-function App() {
+import { useEffect, useState } from 'react';
+import Beep from 'browser-beep';
+
+const INTERVAL = 120000;
+
+const beep = Beep({ frequency: 830, interval: INTERVAL });
+
+function Timer() {
+  const [time, setTime] = useState(0);
+  
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTime(Math.max(time - 1, 0));
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, [time]);
+
+  function handleClick() {
+    beep(2);
+    setTime(INTERVAL / 1000);
+  }
+
   return (
-    <div>Hello, World!</div>
+    <div onClick={handleClick}>{time}</div>
   );
 }
 
-export default App;
+export default Timer;
